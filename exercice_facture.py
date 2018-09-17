4b8344c90e0f2578669ce2652d7de2296249a3ea
Beatrice <beatrice.cs31@gmail.com>
2018-09-17 15:57:58 +0200

    update

--- exercice_facture.py
+++ exercice_facture.py
@@ -1,30 +1,37 @@
+from dataclasses import dataclass
+
+@dataclass
 class Produit:
-    def __init__(self, nom, prix):
-        self.nom = nom
-        self.prix = prix
+    nom : str
+    prix : float = 0.0
 
+@dataclass
+class Lignes_facture:
+    produit : str
+    quantite : float = 0.0
 
-class Ligne_facture:
-    def __init__(self, produit, quantite):
-        self.produit = produit
-        self.nom = produit.nom
-        self.prix = produit.prix
-        self.quantite = quantite
-        self.montant = produit.prix * quantite
+    @property
+    def montant(self):
+        return self.produit.prix * self.quantite
+        #self.montant = produit.prix * quantite
 
-    def afficher_ligne_facture(self):
-        print("Produit:%s Quantite %s Prix %s Montant %s" % (self.nom, self.quantite, self.prix, self.montant,))
+    def afficher_lignes_facture(self):
+        #print("Produit:%s Quantite %s Prix %s Montant %s" % (self.produit.nom, self.quantite, self.produit.prix, self.montant,))
+        print (f"Produit : {self.produit.nom}, Quantité : {self.quantite}, Prix : {self.produit.prix}, Montant : {self.montant}")
+
 
 class Facture:
-    def __init__(self, ligne_facture):
-        self.name = 'INV_2018/0001'
-        self.client = 'toto'
-        self.ligne_facture = ligne_facture
+    def __init__(self, facture_lignes, num_fact = 0, client = None):
+        assert isinstance(num_fact, int), " Numero non-entier, instance de " + str(type(num_fact))
 
-    def calcul_facture(self):
+        self.num_fact = num_fact + 1
+        self.num_fact = 'INV_2018/000' + str(self.num_fact)
+        self.client = client
+        self.facture_lignes = facture_lignes
+
+    def calcul_facture(self, tva = 1.20):
         total = 0.0
-        tva = 1.20
-        for line in facture_ligne:
+        for line in self.facture_lignes:
             total += line.montant
 
         print("Total sans tva=%s"%total)
@@ -32,20 +39,22 @@ class Facture:
         print("Total avec tva=%s"%total)
 
 
+def test_facture():
+    telephone1 = Produit('Iphone6', 800.0)
+    coque = Produit('Coque Protection', 20.0)
+    facture_lignes = []
 
-telephone1 = Produit('iphone6', 800.0)
-coque = Produit('coque', 20.0)
-facture_ligne = []
+    lignes_facture =  Lignes_facture(telephone1, 1)
+    facture_lignes.append(lignes_facture)
+    lignes_facture.afficher_lignes_facture()
 
-ligne_facture =  Ligne_facture(telephone1, 1)
-facture_ligne.append(ligne_facture)
-ligne_facture.afficher_ligne_facture()
+    lignes_facture =  Lignes_facture(coque, 2)
+    facture_lignes.append(lignes_facture)
 
-ligne_facture =  Ligne_facture(coque, 2)
-facture_ligne.append(ligne_facture)
+    lignes_facture.afficher_lignes_facture()
 
-ligne_facture.afficher_ligne_facture()
+    facture = Facture(facture_lignes)
+    facture.calcul_facture()
 
-facture = Facture(ligne_facture)
-facture.calcul_facture()
+test_facture()
 
