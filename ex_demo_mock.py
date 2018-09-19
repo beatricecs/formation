a10b11d963a405201a4557de4ad5d42def04cf9b
Beatrice <beatrice.cs31@gmail.com>
2018-09-19 14:20:52 +0200

    update

--- ex_demo_mock.py
+++ ex_demo_mock.py
@@ -1,6 +1,10 @@
 import requests
-import prettyprinter
+import unittest
+import responses
+from prettyprinter import cpprint
 from dataclasses import dataclass
+
+
 
 @dataclass
 class User:
@@ -11,28 +15,108 @@ class User:
     email: str
     username: str
 
-def
-response = requests.get('https://randomuser.me/api/')
+    @classmethod
+    def create_from_api(cls, result):
+        record = result['results'][0]
+        record_name = record['name']
+        record_login = record['login']
 
-#print(response.status_code)
-result = response.json()
+        user = User(record['gender'], record_name['title'], record_name['first'],
+                    record_name['last'], record['email'], record_login['username']
+                    )
+        return user
 
-prettyprinter.cpprint(result)
-#print(result)
+class APIUnreachableException(Exception):
+    pass
+
+class HttpNotFound(Exception):
+    pass
 
 
-record = result['results'][0]
-record_name = record['name']
-record_login = record['login']
-#print (record['gender'],
-#       record_name['title'],
-#       record_name['first'],
-#       record_name['last'],
-#       record['email'],
-#       record_login['username']
-#       )
-user = User(record['gender'],record_name['title'], record_name['first'],
-            record_name['last'],record['email'],record_login['username']
-            )
+def get_user():
 
-print(user.firstname)
+    try :
+        response = requests.get('https://randomuser.me/api/')
+        print(response.status_code)
+        if response.status_code == 404:
+            raise HttpNotFound()
+        return User.create_from_api(response.json())
+
+        # result = response.json()
+        # cpprint(result)
+        # record = result['results'][0]
+
+
+        # -- deplacement de ce code dans classmethod
+        # record_name = record['name']
+        # record_login = record['login']
+        #
+        # user = User(record['gender'], record_name['title'], record_name['first'],
+        #             record_name['last'], record['email'], record_login['username']
+        #             )
+        # return user
+    except requests.exceptions.ConnectionError as error:
+        print (error)
+        raise APIUnreachableException()
+
+class  GetUserTestCase(unittest.TestCase):
+    # MOCK_USER = {
+    #     'results': [
+    #         {
+    #             'email': 'stephane@wirtel.be',
+    #             'name': {
+    #                 'title': 'Monsieur',
+    #                 'first': 'Stephane',
+    #                 'last': 'Wirtel'
+    #             },
+    #             'login': {'username': 'matrixise'},
+    #             'gender': 'male',
+    #         }
+    #     ]
+    # }
+
+    # ou
+    MOCK_USER = {
+        'results': [{
+            'email': 'stephane@wirtel.be',
+            'name':dict(title='Monsieur', first='Stephane', last='Wirtel'),
+             'login': dict(username='matrixise'),
+           'gender': 'male',
+        }]
+    }
+
+    @responses.activate
+    def test_get_user_works(self):
+        responses.add(responses.GET, 'https://randomuser.me/api/', json=self.MOCK_USER)
+        user = get_user()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.firstname, 'Stephane')
+        self.assertEqual(user.lastname, 'Wirtel')
+
+    #@unittest.skip('in progress') # stopper ce test en cours
+    @responses.activate
+    def test_get_user_not_found(self):
+        responses.add(responses.GET, 'https://randomuser.me/api/', status=404)
+        with self.assertRaises(HttpNotFound):
+            user = get_user()
+
+    #@unittest.skip('in progress') # stopper ce test en cours
+    @responses.activate
+    def test_get_user_server_is_unreachable(self):
+        with self.assertRaises(APIUnreachableException):
+            user = get_user()
+
+
+
+def main():
+    try:
+        user = get_user()
+        cpprint(user)
+    except APIUnreachableException:
+        print ('le serveur est injoignable')
+    except HttpNotFound:
+        print ('Le serveur n''existe pas')
+
+if __name__ == '__main__':
+    unittest.main()
+    main()
